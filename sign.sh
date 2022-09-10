c2dd120a3073ff8127f8cf0687e5844eb255cbde
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-10 20:57:16 +0200

    Fix spacing

--- sign.sh
+++ sign.sh
@@ -1,7 +1,7 @@
- #!/bin/sh
- debug_project=$1
- gpg -b ./target/debug/$debug_project
- gpg --verify ./target/debug/"$debug_project".sig ./target/debug/"$debug_project"
- release_project=$1
- gpg -b ./target/release/"$release_project"
- gpg --verify ./target/release/"$release_project".sig ./target/release/"$release_project"
+#!/bin/sh
+debug_project=$1
+gpg -b ./target/debug/$debug_project
+gpg --verify ./target/debug/"$debug_project".sig ./target/debug/"$debug_project"
+release_project=$1
+gpg -b ./target/release/"$release_project"
+gpg --verify ./target/release/"$release_project".sig ./target/release/"$release_project"

8255ca1d84c0b5ac5c91fc5a7f60f35b03f6eeb5
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-10 21:41:24 +0200

    Restructure

--- sign.sh
+++ sign.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 debug_project=$1
-gpg -b ./target/debug/$debug_project
+gpg -b ./target/debug/"$debug_project"
 gpg --verify ./target/debug/"$debug_project".sig ./target/debug/"$debug_project"
 release_project=$1
 gpg -b ./target/release/"$release_project"

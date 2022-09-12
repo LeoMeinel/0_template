8d945430bba16289446174d2877eccf06e0f299e
Leopold Johannes Meinel <leo@meinel.dev>
2022-09-13 00:08:03 +0200

    Reformat

--- sign.sh
+++ sign.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+###
+# File: sign.sh
+# Author: Leopold Meinel (leo@meinel.dev)
+# -----
+# Copyright (c) 2022 Leopold Meinel & contributors
+# SPDX ID: GPL-3.0-or-later
+# URL: https://www.gnu.org/licenses/gpl-3.0-standalone.html
+# -----
+###
+
 debug_project=$1
 gpg -b ./target/debug/"$debug_project"
 gpg --verify ./target/debug/"$debug_project".sig ./target/debug/"$debug_project"
